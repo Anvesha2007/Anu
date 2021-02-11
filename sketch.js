28409e74aa7a808520b86771e0abd663962a9b46
Anvesha2007 <76908811+Anvesha2007@users.noreply.github.com>
2021-02-11 19:48:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,50 +1,46 @@
-var ms,fs;
-function setup() {
-createCanvas(800,500);
-fs=createSprite(600, 400, 50, 80);
-fs.shapeColor="green"
-fs.debug=true
-ms=createSprite(400, 200, 80, 30);
-ms.shapeColor="blue"
-ms.debug=true
+var tomImg1
+var tomImg2
+var bg
+var  tomImg3
+var  jerryImg1
+var  jerryImg2
+var  jerryImg3
+var tom,jerry
+function preload() {
+    //load the images here
+    bg = loadImage("images/garden.png");
+    tomImg1= loadAnimation("images/cat1.png");
+    tomImg2=loadAnimation("images/cat2.png","images/cat3.png");
+    tomImg3= loadAnimation("images/cat4.png");
+    jerryImg1=loadAnimation("images/mouse1.png");
+    jerryImg2= loadAnimation("images/mouse2.png","images/mouse3.png");
+    jerryImg3=loadAnimation("images/mouse4.png");
+    
 }
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+tom=createSprite(870,600,100,100)
+jerry=createSprite(200,600,100,100)
+tom.addAnimation("tomSleeping",tomImg1)
+jerry.addAnimation("jerryStanding",jerryImg1)
+tom.scale=0.2
+jerry.scale=0.2
+
+}
+
 function draw() {
-  background("pink"); 
-  ms.x=World.mouseX
-  ms.y=World.mouseY
+    
+    
+    background(bg);
+    //Write condition here to evalute if tom and jerry collide
 
-if (ms.x-fs.x<ms.width/2+fs.width/2
-  && fs.x-ms.x<ms.width/2+fs.width/2
-  &&  ms.y-fs.y<ms.height/2+fs.height/2
-  &&  fs.y-ms.y<ms.height/2+fs.height/2
-
-  
-  )
-
-{
-  ms.shapeColor="orange"
-  fs.shapeColor="yellow"
-}
-else{
-  fs.shapeColor="green"
-  ms.shapeColor="blue"
-}
- 
-
-
-
-
-  drawSprites();
+    drawSprites();
 }
 
 
+function keyPressed(){
 
-
-
-
-
-
-
-
-
-
+  //For moving and changing animation write code here
+}
