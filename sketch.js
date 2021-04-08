a4010426fa0881405598ff7c1bc4cd7db726c24a
Anvesha2007 <76908811+Anvesha2007@users.noreply.github.com>
2021-04-08 12:39:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,36 @@
-var starImg, fairyImg, bgImg;
-var fairy , fairyVoice;
-var star, starBody;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-function preload()
-{
-	starImg = loadImage("images/star.png");
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
-	bgImg = loadImage("images/starNight.png");
-	fairyVoice = loadSound("sound/JoyMusic.mp3");
-
-}
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+var paper 
 
 function setup() {
-	createCanvas(800, 750);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
-	// fairyVoice.play();
-
-	fairy = createSprite(130, 520);
-	fairy.addAnimation("fairyflying",fairyImg);  
-	fairy.scale =0.25;
-
-	star = createSprite(650,30);
-	star.addImage(starImg);
-	star.scale = 0.2;
 
 	engine = Engine.create();
 	world = engine.world;
-
-	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
-	World.add(world, starBody);
 	
-	Engine.run(engine);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
 
+	Engine.run(engine);
+  
 }
 
 
 function draw() {
-  background(bgImg);
+  rectMode(CENTER);
+  background(230);
+ 
 
-  drawSprites();
+  groundObject.display();
+  dustbinObj.display();
 
 }
 
-function keyPressed() {
-	//write code here
-}
